25a1f74aed2bd2466c9c712a4d033436c8cfc1ee
Jiarui <ugvgn@student.kit.edu>
2025-05-10 18:27:34 +0200

    Example project

---
+++ TaskExample/hello.cpp
@@ -0,0 +1,14 @@
+//
+// Created by jiaruidai on 25-5-10.
+//
+
+#include "hello.h"
+#include <iostream>
+
+// using namespace std;
+
+int main(){
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
---
+++ TaskExample/hello.h
@@ -0,0 +1,13 @@
+//
+// Created by jiaruidai on 25-5-10.
+//
+
+#ifndef HELLO_H
+#define HELLO_H
+#include <iostream>
+
+
+int main();
+
+#endif //HELLO_H
+
